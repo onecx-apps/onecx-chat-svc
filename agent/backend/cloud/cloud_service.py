7879f9f380aa003bfdf0a85a628e67b2db3147f6
Michael Gloeckner <michael.gloeckner@capgemini.com>
2023-12-12 13:39:19 +0100

    Cleanup for quarkus model

--- agent/backend/cloud/cloud_service.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class CloudService(ABC):
-    @abstractmethod
-    def download_files_from_bucket(self) -> None:
-        pass
--- agent/data_model/response_model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-import enum
-
-class SimpleDetailMessage(BaseModel):
-    detail: str = Field(None, description="response message", max_length=10000)
-    
-# Additional Responses
-NOT_FOUND = {"model": SimpleDetailMessage, "description": "Requested or used item not found."}
