a56bab7bafddb6de8fe4239c395310f3facc1919
Bartosz <bartosz.brynski@capgemini.com>
2024-10-31 10:31:06 +0100

    add ai-svc to docker-compose

--- db/create-onecx-databases.sql
+++ db/create-onecx-databases.sql
@@ -12,3 +12,10 @@ DROP DATABASE IF EXISTS "onecx-chat";
   CREATE USER onecx_chat WITH PASSWORD 'onecx_chat';
 CREATE DATABASE "onecx-chat" WITH OWNER = onecx_chat;
 
+/*
+ai-specific part not required if use only onecx-chat-svc
+*/
+DROP DATABASE IF EXISTS "onecx-ai";
+  DROP ROLE IF EXISTS onecx_ai;
+  CREATE USER onecx_ai WITH PASSWORD 'onecx_ai';
+CREATE DATABASE "onecx-ai" WITH OWNER = onecx_ai;
